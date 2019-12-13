a6460511fb54e562eab8c6c95e00eb1ef768929d
Olivia Kesler <oakesler@protonmail.ch>
2019-12-13 15:21:20 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -10,7 +10,7 @@ class CreateHauntedHouses< ActiveRecord::Migration[5.2]
       t.string :family_friendly
       t.integer :opening_date
       t.integer :closing_date
-      t.string :long_description
+      t.string :description
     end
   end
 end
