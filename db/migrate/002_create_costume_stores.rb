b896b3537dfde7cd7b73db0d9c1a1ac3b2fa3434
Olivia Kesler <oakesler@protonmail.ch>
2019-12-13 15:32:24 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -13,7 +13,7 @@ class CreateCostumeStores< ActiveRecord::Migration[5.2]
       t.string :location
       t.integer :costume_inventory
       t.integer :employee_count
-      t.string :in_business
+      t.boolean :in_business
       t.datetime :opening_time
       t.datetime :closing_time
     end
