bb57ec9b3699e9b1af5b80a59ecd2284c99aa11a
Olivia Kesler <oakesler@protonmail.ch>
2019-12-13 15:28:17 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -2,7 +2,7 @@ class CreateCostumes < ActiveRecord::Migration[5.2]
   def change
     create_table :costumes do |t|
       t.string :name
-      t.string :price
+      t.integer :price
       t.string :size
       t.string :image_url
     end
